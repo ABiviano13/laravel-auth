2f1e3800891f3840d9a55b34769488bd5dd64f4a
ABiviano13 <abiviano0@outlook.com>
2023-04-20 16:16:40 +0200

    FEAT migration + Faker (Not completed )

--- database/migrations/2023_04_20_135601_create_posts_table.php
+++ database/migrations/2023_04_20_135601_create_posts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->unique();
+            $table->text('content')->nullable();
+            $table->string('slug');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
 {
@@ -12,7 +13,7 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
     }
